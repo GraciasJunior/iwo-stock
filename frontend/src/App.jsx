9ebb0853427915c551a9579c0bc34c23f82f09aa
gracias_jr <graciasdagadgc@gmail.com>
2024-10-03 16:51:16 +0100

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './index.css';
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-red-300">
+       IWO STOCK V.0.0.0
+      </div>
+    </>
+  )
+}
+
+export default App
